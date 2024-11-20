bfc33059babb892455ebd06eba3c70bfedb1ea12
Jonathan Martins <jmar@ip-192-168-1-59.eu-west-1.compute.internal>
2024-11-20 19:45:16 +0000

    refactor(api): change how to perform queries

--- packages/api/app.ts
+++ packages/api/app.ts
@@ -50,5 +50,46 @@ export default function initializeExpressApp(mongoClient: MongoClient) {
     }
   });
 
+  app.get("/accommodations", async (req, res) => {
+    const { search } = req.query;
+    try {
+      await mongoClient.connect();
+      console.log("connected");
+      const db = mongoClient.db();
+
+      const hotels = await db
+        .collection("hotels")
+        .find({
+          $or: [
+            { chain_name: { $regex: search, $options: "i" } },
+            { hotel_name: { $regex: search, $options: "i" } },
+            { city: { $regex: search, $options: "i" } },
+            { country: { $regex: search, $options: "i" } },
+          ],
+        })
+        .toArray();
+
+      const cities = await db
+        .collection("cities")
+        .find({
+          $or: [{ name: { $regex: search, $options: "i" } }],
+        })
+        .toArray();
+
+      const countries = await db
+        .collection("countries")
+        .find({
+          $or: [
+            { country: { $regex: search, $options: "i" } },
+            { countryisocode: { $regex: search, $options: "i" } },
+          ],
+        })
+        .toArray();
+      res.send({ hotels, cities, countries });
+    } finally {
+      await mongoClient.close();
+    }
+  });
+
   return app;
 }
